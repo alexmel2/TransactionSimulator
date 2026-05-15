ea874fd39446aa58566a1d095464f2656e725364
alexmel2 <alexmel2@gmail.com>
2026-05-15 16:52:23 +0300

    עדכון פרויקט ראשון

---
+++ TransactionSimulatorAPi/TransactionSimulator.Api/Program.cs
@@ -0,0 +1,80 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+
+using Serilog;
+using System.Text;
+using TransactionSimulator.Api.Validators;
+using TransactionSimulator.Application;
+using TransactionSimulator.Domain.Config;
+using TransactionSimulator.Domain.Interfaces;
+using TransactionSimulator.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// --- 1. Serilog Setup ---
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+try
+{
+    Log.Information("Starting Shva Simulator API...");
+
+  
+
+    // --- 4. Service Registrations ---
+    builder.Services.Configure<AppSettings>(builder.Configuration);
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddApplication(builder.Configuration);
+
+    var appSettings = builder.Configuration.Get<AppSettings>();
+
+    #region CorsConfig
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(appSettings.CorsConfig.PolicyName,
+            policy => policy.WithOrigins(appSettings.CorsConfig.AllowedOrigins)
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+    });
+    #endregion
+
+    #region FluentValidation
+    builder.Services.AddFluentValidationAutoValidation();
+    builder.Services.AddValidatorsFromAssemblyContaining<TransactionRequestValidator>();
+    #endregion
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // --- 5. Middleware Pipeline ---
+    app.UseCors(appSettings?.CorsConfig?.PolicyName);
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication(); // Identification
+    app.UseAuthorization();  // Permissions
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
---
+++ TransactionSimulatorAPi/TransactionSimulator.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionSimulator.Domain.Interfaces;
+using TransactionSimulator.Infrastructure.Caching;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TransactionSimulator.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            ArgumentNullException.ThrowIfNull(connectionString, "Connection string 'DefaultConnection' not found in configuration.");
+            // ---  DbContext Configuration ---
+            services.AddScoped<IApplicationDbContext, AppDbContext>();
+
+            services.AddDbContext<AppDbContext>(options =>
+                      options.UseSqlServer(
+                     connectionString,
+                      b => b.MigrationsAssembly("TransactionSimulator.Infrastructure")));
+            services.AddMemoryCache();
+            services.AddHostedService<RegionCacheService>();
+            // ---  Identity Configuration ---
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            // --- 3. JWT Authentication ---
+            var jwtSettings = configuration.GetSection("Jwt");
+            var key = Encoding.ASCII.GetBytes(jwtSettings["Key"] ?? "YourDefaultSecretKeyForDevelopment");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+            return services;
+        }
+    }
+}
